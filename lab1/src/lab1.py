cc5b4536616672edd75df334ae9bdeb6f652e819
breadrock1 <breadrock1@gmail.com>
2019-11-09 21:43:11 +0300

    Now it works on jupyter notebook

--- lab1/src/lab1.py
+++ lab1/src/lab1.py
@@ -1,17 +1,22 @@
 """
-Буду предсказывать оценки G4 студентов, из представленных характеристик в данной таблице:
-    Medu - mother's education (numeric: 0 - none, 1 - primary education (4th grade), 2 – 5th to 9th grade, 3 – secondary education or 4 – higher education)
-    Fedu - father's education (numeric: 0 - none, 1 - primary education (4th grade), 2 – 5th to 9th grade, 3 – secondary education or 4 – higher education)
-    studytime - weekly study time (numeric: 1 - <2 hours, 2 - 2 to 5 hours, 3 - 5 to 10 hours, or 4 - >10 hours)
-    paid - extra paid classes within the course subject (Math or Portuguese) (binary: yes or no)
-    higher - wants to take higher education (binary: yes or no)
-    internet - Internet access at home (binary: yes or no)
-    G1 - first period grade (numeric: from 0 to 20)
-    G2 - second period grade (numeric: from 0 to 20)
-    G3 - final grade (numeric: from 0 to 20, output target)
+This database contains 76 attributes, but all published experiments refer to using a subset of 14 of them.
+In particular, the Cleveland database is the only one that has been used by ML researchers to this date.
+Dataset consists:
+        > 1. age
+        > 2. sex
+        > 3. chest pain type (4 values)
+        > 4. resting blood pressure
+        > 5. serum cholestoral in mg/dl
+        > 6. fasting blood sugar > 120 mg/dl
+        > 7. resting electrocardiographic results (values 0,1,2)
+        > 8. maximum heart rate achieved
+        > 9. exercise induced angina
+        > 10. oldpeak = ST depression induced by exercise relative to rest
+        > 11. the slope of the peak exercise ST segment
+        > 12. number of major vessels (0-3) colored by flourosopy
+        > 13. thal: 3 = normal; 6 = fixed defect; 7 = reversable defect
 
-    Цель работы: Найти закономерность между данными характеристиками, чтобы прогнозировать оценки студента
-
+    The goal: Trying to predict heart disease in the patient by thic dataset.
 """
 
 import pandas as pd
@@ -22,10 +27,11 @@ from IPython.core.pylabtools import figsize
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
+
 # No warnings about setting value on copy of slice
 pd.options.mode.chained_assignment = None
-# Display up to 60 columns of a dataframe
-pd.set_option('display.max_columns', 30)
+# Display up to 14 columns of a dataframe
+pd.set_option('display.max_columns', 14)
 # Set default font size
 plt.rcParams['font.size'] = 24
 sns.set(font_scale=2)
@@ -51,6 +57,7 @@ def terminator_training(data):
         train_scores[n] = current_train_score
         test_scores[n] = current_test_score
 
+    # TODO: needs fix TypeError!!!
     plt.plot(train_scores.keys(), train_scores.values(), label='Обучающая выборка')
     plt.plot(test_scores.keys(), test_scores.values(), label='Тестовая выборка')
     plt.xlabel('Количество соседей')
@@ -97,10 +104,7 @@ def visual_histograms(column, bins, xlab, title):
 
 # Drop selected tables which play no role
 def drop_extra_columns(data):
-    extra_columns = ['school', 'sex', 'age', 'address', 'famsize', 'Pstatus', 'Mjob',
-                     'Fjob', 'guardian', 'reason', 'guardian', 'traveltime', 'failures',
-                     'schoolsup', 'famsup', 'activities', 'nursery', 'romantic',
-                     'famrel', 'freetime', 'goout', 'Dalc', 'Walc', 'health', 'absences']
+    extra_columns = ['sex', 'thalach', 'exang', 'oldpeak', 'ca']
     data = data.drop(columns=list(extra_columns))
 
     return data
@@ -112,21 +116,27 @@ def set_correct_types(data):
     booles = {'no': 0, 'yes': 1}
 
     for col in list(data.columns):
-        if ('Medu' in col or 'Fedu' in col or 'studytime' in col
-                or 'G1' in col or 'G2' in col or 'G3' in col):
+        if ('Medu' in col or 'Fedu' in col or 'traveltime' in col or 'studytime' in col
+                or 'failures' in col or 'famrel' in col or 'freetime' in col or 'goout' in col
+                or 'Dalc' in col or 'Walc' in col or 'G1' in col or 'G2' in col or 'G3' in col
+                or 'health' in col):
             data[col] = data[col].astype(int)
-        elif 'paid' in col or 'higher' in col or 'internet' in col:
+
+        elif ('schoolsup' in col or 'famsup' in col or 'paid' in col or 'activities' in col
+              or 'nursery' in col or 'higher' in col or 'internet' in col or 'romantic' in col):
             data[col].replace(booles, inplace=True)
-            data[col] = data[col].astype(bool)
-        else: continue
+            data[col] = data[col].astype(int)
+
+        else:
+            continue
 
     return data
 
 
 def main():
-    data = pd.read_csv("/home/akimg/Myprojects/ML_NeighborsClassifier-master/lab1/dataset/student-mat.csv")
-    data = set_correct_types(data)
+    data = pd.read_csv("/home/akimg/Myprojects/ML_NeighborsClassifier-master/lab1/src/dataset/student-mat.csv")
     data = drop_extra_columns(data)
+    data = set_correct_types(data)
 
     visual_histograms(data['studytime'], 12, 'Study time', 'Time of study')
     visual_histograms(data['G3'], 20, 'Grades', 'Final grade')
